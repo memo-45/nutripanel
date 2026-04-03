f283e27c783685f2fffd391ac83ba046dca70bdb
memo-45 <m_girginler@hotmail.com>
2026-04-03 23:50:38 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,28 +1,18 @@
-const CACHE = 'nutripanel-v7';
-const BASE = '/nutripanel';
+const CACHE = 'nutripanel-v8';
 
 self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(CACHE).then(function(cache) {
-      return cache.addAll([BASE + '/', BASE + '/index.html']);
-    })
-  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', function(e) {
   e.waitUntil(
     caches.keys().then(function(keys) {
-      return Promise.all(keys.filter(function(k) { return k !== CACHE; }).map(function(k) { return caches.delete(k); }));
+      return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));
     })
   );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    fetch(e.request).catch(function() {
-      return caches.match(e.request);
-    })
-  );
+  e.respondWith(fetch(e.request).catch(function(){return caches.match(e.request);}));
 });
